690abf5851686c6eb708e62f59a7f14a7e42027e
Arkaenaz <flooper2234@gmail.com>
2023-02-19 00:43:38 +0800

    added the game start to main

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,21 @@
 /****************************************************
-Group: 		
-Section:	
-Members:	
+Group: Group 4
+Section: S11
+Members: ARGUELLES, Joachim
+         CABLAYAN, Shane Laurenze
+         URSUA, James
 ****************************************************/
+
+#include "iostream"
+
+#include "Utility/Interface.cpp"
+
+#include "Controller/Game.cpp"
+
+int main() {
+
+    Game CGame = Game();
+    CGame.title();
+
+    return 0;
+}
