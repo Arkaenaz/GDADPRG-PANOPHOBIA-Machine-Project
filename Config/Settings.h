f795383c96f8bf6a2b55baba5e4a4705f01eb1cf
Arkaenaz <flooper2234@gmail.com>
2023-02-19 00:41:26 +0800

    Added Settingsto easily change aspects of the code

---
+++ Config/Settings.h
@@ -0,0 +1,16 @@
+/****************************************************
+Author: 
+Date Created: February 18, 2022, 10:57 PM
+Description: 
+****************************************************/
+
+#ifndef CONFIG_SETTINGS_H
+#define CONFIG_SETTINGS_H
+
+bool CLEAR_CONSOLE = false;
+bool SYSTEM_TEXT = true;
+
+int SCREEN_WIDTH = 70;
+int SCREEN_HEIGHT = 41;
+
+#endif
