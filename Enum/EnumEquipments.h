2578a13cc385ca87af498c605f6968d365d0c2fa
Arkaenaz <flooper2234@gmail.com>
2023-02-19 23:24:17 +0800

    added enum for equipments

---
+++ Enum/EnumEquipments.h
@@ -0,0 +1,14 @@
+#ifndef ENUM_EQUIPMENT_H
+#define ENUM_EQUIPMENT_H
+
+enum class Equipments : int {
+    EMF_READER = 0,
+    FLASHLIGHT,
+    VIDEO_CAMERA,
+    SANITY_PILLS,
+    THERMOMETER,
+    NOTEBOOK,
+    GLOWSTICK
+};
+
+#endif
