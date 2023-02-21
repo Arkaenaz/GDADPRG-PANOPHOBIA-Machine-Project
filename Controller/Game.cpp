06dd82e8c9a9e9089be9a213e0c0198cb6011e96
Arkaenaz <flooper2234@gmail.com>
2023-02-22 00:08:55 +0800

    slight modifications

--- Controller/Game.cpp
+++ Controller/Game.cpp
@@ -19,31 +19,32 @@ Game::Game() {
 void Game::title() {
     char cInput;
     if (SYSTEM_TEXT)
-        std::cout << "Entered title()." << std::endl;
+        std::cout << "Entered Game::title()." << std::endl;
     do {
         if (CLEAR_CONSOLE)
             system("cls");
         this->CInterface.printTitleScreen();
         cInput = this->CInterface.scanChar();
         switch(cInput) {
+            case '0':
+                std::cout << "Quitting..." << std::endl;
+                break;
             case '1':
                 this->lobby();
                 break;
-            case '2':
-                std::cout << "Quitting..." << std::endl;
-                break;
+            
         }
-    } while (cInput != '2');
+    } while (cInput != '0');
 }
 
 void Game::lobby() {
     char cInput;
     if (SYSTEM_TEXT)
-        std::cout << "Entered lobby()." << std::endl;
+        std::cout << "Entered Game::lobby()." << std::endl;
     do {
         if (CLEAR_CONSOLE)
             system("cls");
-        std::cout << "[0] Go back" << std::endl;
+        this->CInterface.printLobbyScreen();
         cInput = this->CInterface.scanChar();
     } while (cInput != '0');
 }
