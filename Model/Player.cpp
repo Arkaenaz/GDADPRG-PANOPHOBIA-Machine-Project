5e6e8acdf36e5cfe66e59694054be79a3b927875
ALTelesto <83495459+ALTelesto@users.noreply.github.com>
2023-02-22 00:53:34 +0800

    Start of Player class
    + created Player.cpp
    = started Player.h

---
+++ Model/Player.cpp
@@ -0,0 +1,9 @@
+#include "Player.h"
+
+using namespace gamePlayer;
+
+
+
+float Player::getSanity(){
+    return this->fSanity;
+}
--- Model/Player.h
+++ Model/Player.h
@@ -1,5 +1,34 @@
 /****************************************************
-Author: 
-Date Created: 
-Description: 
+Author: Joachim Gabriel R. Arguelles
+Date Created: February 21, 2023
+Description: Player class
 ****************************************************/
+
+#ifndef GAME_ELEMENTS_PLAYER_H
+#define GAME_ELEMENTS_PLAYER_H
+
+namespace gamePlayer{
+
+    using namespace std;
+
+    class Player{
+        private:
+            string strName;
+            float fSanity;
+            bool isDead;
+
+        public:
+            Player();
+
+            void pan();
+            void interact();
+            void changeEquipment();
+            void viewJournal();
+
+            void drainSanity(float PASSIVE_DRAIN);
+
+            float getSanity();
+    };
+}
+
+#endif
