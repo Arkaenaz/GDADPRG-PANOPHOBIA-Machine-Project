faf8b9af217769e9d29ba0896f0dab0497dfcabf
Kirvvs <jb.ursua13@gmail.com>
2023-02-28 23:33:01 +0800

    Van cpp

--- Screens/AreaProper/Rooms/Van.h
+++ Screens/AreaProper/Rooms/Van.h
@@ -3,3 +3,27 @@ Author:
 Date Created: 
 Description: 
 ****************************************************/
+
+#ifndef GAME_ELEMENTS_VAN_H
+#define GAME_ELEMENTS_VAN_H
+
+#include "../Room.h"
+
+namespace gameArea{
+    class Van{
+
+        public:
+            Van();
+        
+
+        public:
+
+            float sanityPlayer(float fSanity);
+            float sanityAverage(float fSanity1,float fSanity2,float fSanity3,float fSanity4);
+            void endRun();
+    };
+
+}
+
+#endif
+
