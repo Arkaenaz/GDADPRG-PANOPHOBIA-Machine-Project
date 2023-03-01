a1a0235dd150b0ad2b9510dfd83cf1317cbb47ed
ALTelesto <83495459+ALTelesto@users.noreply.github.com>
2023-03-01 18:15:44 +0800

    Update FoyerWalls.cpp

--- Screens/AreaProper/Rooms/Wall/FoyerWalls.cpp
+++ Screens/AreaProper/Rooms/Wall/FoyerWalls.cpp
@@ -3,20 +3,20 @@
 using namespace gameArea;
 using namespace gameInteractable;
 
-FoyerWall1::FoyerWall1():Wall(){
+FoyerWall1::FoyerWall1():WallOBJ(){
     this->vecDecor.push_back(Door(1,0));
     this->createDecor(6);
 }
 
-FoyerWall2::FoyerWall2():Wall(){
+FoyerWall2::FoyerWall2():WallOBJ(){
     this->createDecor(3);
 }
 
-FoyerWall3::FoyerWall3():Wall(){
+FoyerWall3::FoyerWall3():WallOBJ(){
     this->vecDecor.push_back(Door(5,0));
     this->vecDecor.push_back(Lightswitch());
 }
 
-FoyerWall4::FoyerWall4():Wall(){
+FoyerWall4::FoyerWall4():WallOBJ(){
     this->vecDecor.push_back(Door(2,3));
 }
